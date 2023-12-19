8d3b487a49d4a0defcec24d9e4c8cd44b92b6701
esteban3221 <esteban-oe100@hotmail.com>
2023-12-19 11:09:49 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "ventana_main.hpp"
 
 int main(int argc, char *argv[])
-{
+{ 
     auto app = Gtk::Application::create("org.gtkmm.example");
     return app->make_window_and_run<ventana_main>(argc, argv);
 
